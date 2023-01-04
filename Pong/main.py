8f2cbbdb8d4ccd9febfec5ed14fb8055d3b86147
5J0987897 <morrison.taylor.j@gmail.com>
2023-01-04 15:53:52 -0500

    ball

--- Pong/main.py
+++ Pong/main.py
@@ -1,5 +1,9 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,8 +11,13 @@ screen.bgcolor("black")
 screen.title("My Pong Game")
 screen.tracer(0)
 
+scoreboard = Scoreboard()
+
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+
+first_ball = Ball((0, 0))
+first_ball.move()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,7 +27,19 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(first_ball.move_speed)
     screen.update()
+    first_ball.move()
 
+    if first_ball.ycor() > 280 or first_ball.ycor() < -280:
+        first_ball.bounce_y()
+    elif first_ball.distance(r_paddle) < 50 and first_ball.xcor() > 320 or first_ball.distance(l_paddle) < 50 and first_ball.xcor() < -320:
+        first_ball.bounce_x()
+    elif first_ball.xcor() > 400:
+        first_ball.reset_position()
+        scoreboard.l_point()
+    elif first_ball.xcor() < -400:
+        first_ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
