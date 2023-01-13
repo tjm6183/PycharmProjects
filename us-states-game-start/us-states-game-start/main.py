3be13d5fb313777827a916f9f116cf00f937067f
5J0987897 <morrison.taylor.j@gmail.com>
2023-01-13 11:20:31 -0500

    50 States completed

---
+++ us-states-game-start/us-states-game-start/main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+
+        break
+    if answer_state in all_states and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+for state in data["state"]:
+    for guessed_state in guessed_states:
+        if state == guessed_state:
+            all_states.remove(f"{guessed_state}")
+
+print("These are the states you should study:")
+for state in all_states:
+    print(state)
+
+screen.exitonclick()
