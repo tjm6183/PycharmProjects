cbe80dc2e75d35fc29f95e6a99a1c7195cb98c79
5J0987897 <morrison.taylor.j@gmail.com>
2023-01-17 09:31:38 -0500

    Comprehension final

--- NATO-alphabet-start/NATO-alphabet-start/main.py
+++ NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -23,5 +23,13 @@ for (index, row) in student_data_frame.iterrows():
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
