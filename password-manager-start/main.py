19d4c76d37706dc24250f7dc11bb72855fe87860
5J0987897 <morrison.taylor.j@gmail.com>
2023-01-26 15:05:24 -0500

    Password Manager

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,5 +1,45 @@
+import tkinter
+from tkinter import *
+
+WHITE = "#fff"
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save_password():
+    pass
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(row=0, column=1)
+
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(row=2, column=0)
+username_entry = Entry(width=35)
+username_entry.grid(row=2, column=1, columnspan=2)
+username_entry.insert(0, "morrison_taylor@aol.com")
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+password_entry = Entry(width=35)
+password_entry.grid(row=3, column=1, columnspan=2)
+
+password_button = Button(text="Generate Password")
+password_button.grid(row=3, column=2)
+
+add_button = Button(width=35, text="Add")
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
